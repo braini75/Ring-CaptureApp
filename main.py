ed855799e2e5267f1e95c64d1cc0f82272471ef2
Thomas Koch <braini@illuxt-debian10.clondiag.jena>
2020-01-09 12:16:16 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+"""
+
+"""
+import logging
+import capture_server
+from configparser import ConfigParser
+import ast
+import threading
+
+config = ConfigParser()
+config.read('etc/settings.cfg')
+
+log_file = config['logging'].get('access_log_file')
+level = config['logging'].get('log_level')
+port_list = ast.literal_eval(config['main'].get('port_list'))
+
+def main():
+    # Get a list of all sections
+    logging.basicConfig(filename=log_file, level=logging.getLevelName(level))
+    logging.info('Started')
+    # do something
+    for port in port_list:
+        srv = threading.Thread(target=capture_server.run_server, args=("", port,))
+        srv.start()
+        #srv.join()
+        #capture_server.run_server("", port)
+
+    srv.join()
+    logging.info('Finished')
+
+
+if __name__ == '__main__':
+    main()
