0c6c543d0a971ee52161b37c7e205bb24ca9ae12
Thomas K <braini@sonnenkinder.org>
2023-06-29 22:43:16 +0200

    pylint fixes

--- capture_server.py
+++ capture_server.py
@@ -19,7 +19,7 @@ class MyServer(BaseHTTPRequestHandler):
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
-        self.wfile.write(bytes("<html><head><title>
+        self.wfile.write(bytes("<html><head><title> \
                                https://pythonbasics.org</title></head>", "utf-8"))
         self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         logging.info(self.path)
@@ -45,4 +45,4 @@ def run_server(host_name="", server_port=8080):
 
 
 if __name__ == "__main__":
-    run_server(hostName=HOSTNAME, serverPort=PORT)
+    run_server(HOSTNAME, PORT)
